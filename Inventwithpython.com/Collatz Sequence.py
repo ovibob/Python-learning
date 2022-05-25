952d3fe8ee2685f48e0725ee0f00c82ff613fc10
ovibob <94010718+ovibob@users.noreply.github.com>
2022-05-25 20:07:45 +0300

    A math problem....(Collatz sequence)

---
+++ Inventwithpython.com/Collatz Sequence.py
@@ -0,0 +1,29 @@
+import sys
+import time
+
+print('''
+    The Collatz sequence (or "3n + 1" problem) is a sequence of numbers produced from a starting
+    number n, following three rules:
+        1) If n is even, the next number n is n / 2.
+        2) If n is odd, the next number n is 3n + 1.
+        3) If n is 1, stop. Otherwise, repeat.
+    It is generally thought, but so far not mathematically proven, that
+    every starting number eventually terminates at 1. ''')
+
+print('Enter a starting number (greater than 0) or QUIT: ')
+response = input('> ')
+
+if not response.isdecimal() or response == 0:
+    print('You must enter an integer greater than 0!')
+    sys.exit()
+
+n = int(response)
+print(n, end='', flush = True)
+while n != 1:
+    if n % 2 == 0:
+        n = n // 2
+    else:
+        n = 3*n + 1
+    print(', ' + str(n), end='', flush=True)
+    time.sleep(0.5) # delaying numbers printing with 0.5 sec in between
+print()
